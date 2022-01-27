b660cbfab337827ce67ebffffac2721e8ee3ca4c
wiliami <wiliaup97@gmail.com>
2022-01-27 15:50:49 -0400

    adicionando update code

--- anatomy-4.js
+++ anatomy-4.js
@@ -2,4 +2,12 @@
     const x = 3
     console.log(`Result: ${a + b + c}`)
     console.log(x)
-}) (1, 2, 3)
+}) (1, 2, 3);
+
+
+(function (a, b, c) {
+    const x = 300
+    console.log(`Result: ${a + b + c}`)
+    console.log(x)
+}) (1, 2, 3);
+

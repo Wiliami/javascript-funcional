b306aba337b7b096ae15361b24fc6a5b282bf0a5
wiliami <wiliaup97@gmail.com>
2022-03-13 01:07:22 -0400

    update

--- loop.js
+++ loop.js
@@ -1,20 +1,24 @@
-const produtos = [
-    { produto: 'café', preco: 7 },
-    { produto: 'açucar', preco: 23 },
-    { produto: 'feijão', preco: 9},
-    { produto: 'feijão', preco: 9}
+// const produtos = [
+//     { produto: 'café', preco: 7 },
+//     { produto: 'açucar', preco: 23 },
+//     { produto: 'feijão', preco: 9},
+//     { produto: 'feijão', preco: 9}
 
-]
-const infoArray = (index, produto, array) => {
-    console.log(index, produto, array);
-};
-produtos.forEach(infoArray);
+// ]
+// const infoArray = (index, produto, array) => {
+//     console.log(index, produto, array);
+// };
+// produtos.forEach(infoArray);
 
 
 
-const n = 8;
-for(let i = 1; i <= 10; i ++) {
-    console.log (`${i} x ${n} = ${i*n}`);
+// const n = 8;
+// for(let i = 1; i <= 10; i ++) {
+//     console.log (`${i} x ${n} = ${i*n}`);
+// }
+
+
+for(let idade = 1; idade <= 10; idade++) {
+    console.log(idade)
 }
-
 
